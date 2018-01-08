8cb1113ed291e91cc89f5018e41ebaa09a5ca278
Gavin Brown <scareb15@gmail.com>
2018-01-07 21:11:01 -0700

    deleteed unnecessary

--- knexfile.js
+++ knexfile.js
@@ -9,37 +9,9 @@ module.exports = {
      database: 'interview'
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: process.env.DATABASE_URL 
+  
   }
-
 };
